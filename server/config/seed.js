695c9522738f63794dfec87f21bcdcf694340d8a
Paulo Luan <pauloluan.inova@gmail.com>
2015-07-25 15:40:48 -0300

    fixed initial data

--- server/config/seed.js
+++ server/config/seed.js
@@ -12,25 +12,25 @@ Denunciation.find({}).remove(function() {
     Denunciation.create({
         name: "Dona Amélia",
         address: "Rua do desperdicío, 111",
-        date: "25/07/2015",
+        date: new Date(),
         hour: "15:35",
         description: "Uma senhora meio rabugenta lavando a rua, repete o mesmo ato todo santo dia"
     }, {
         name: "Seu Rubens",
         address: "Rua do desperdicío, 345",
-        date: "24/07/2015",
+        date: new Date(),
         hour: "10:00",
         description: "Todo dia o seu Rubens Lava o carro"
     }, {
         name: "Carlos Alberto de Nóbrega",
         address: "Rua da praça, 111",
-        date: "15/07/2015",
+        date: new Date(),
         hour: "23:00",
         description: "Várias piadas sem graça sobre desperdício de Água."
     }, {
         name: "Mussun Ipsum",
         address: "Rua dos Loren Ipsum, 111",
-        date: "25/07/2015",
+        date: new Date(),
         hour: "15:35",
         description: "Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis."
     });
