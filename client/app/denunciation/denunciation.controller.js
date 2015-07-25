7e0920173c4ad4233f1da2c1dfe6b1f53ba49eee
Gustavo Furtado <gustavo.foa@gmail.com>
2015-07-25 15:40:53 -0300

    Denunciation details page

--- client/app/denunciation/denunciation.controller.js
+++ client/app/denunciation/denunciation.controller.js
@@ -1,6 +1,13 @@
 'use strict';
 
 angular.module('velorioAguaApp')
-    .controller('DenunciationCtrl', function($scope) {
+    .controller('DenunciationCtrl', function($scope, $http, $stateParams) {
         $scope.message = 'Hello';
+        console.log($stateParams.id);
+
+
+        $http.get('/api/denunciations/'+$stateParams.id).success(function(denunciation) {
+            $scope.denunciation = denunciation;
+        });
     });
+});
